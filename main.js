4643a270591d128d3fd96a84f26bc7f1907d4de7
manuzinha200 <emanuele.nascimento.oliveira@escola.pr.gov.br>
2024-10-11 22:53:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -27,4 +27,33 @@ function calcule(tempoObjetivo) {
       let tempoAll = new Date();
       let tempoFinal = tempoObjetivo - tempoAtual;
       let segundos = Math.floor(tempoFinal / 1000);
-      let minutos = Math.floor(
+      let minutos = Math.floor(segundos / 60);
+     let horas = Math.floor(minutos / 60);
+     let dias = Math.floor(horas / 24    );
+
+     segundos %= 60;
+     minutos %= 60;
+     horas %= 24;
+     if (tempofinal > 0){
+          return [dias,horas,minutos,segundos];
+     } else {
+          return [0,0,0,0];
+     }
+}
+
+function atualizaCronomet ro () {
+     for (let i=0; i<contadores . length;it+) {
+          document. getElementById("dias "+i).textContent = calculaTempo (tempos [i ])[O];
+document. getElementById ("horas "+i) .textContent = calculaTempo (tempos [i]) [1];
+document . getElementById ("min"+i) . textContent = calculaTempo (tempos [i]) [2];
+document. getElementById ("seg"+i) .textContent = calculaTempo (tempos [i]) [3];
+     }
+}
+
+function comecaCronometro (){
+atualizaCronometro ();
+setInterval (atualizaCronometro, 1000 );
+}
+
+comecaCronometro();
+
